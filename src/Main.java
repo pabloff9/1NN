16347734766bc377f520deb6c09d770f656bdaa0
Pablo Almeida <pabloalmeidaff9@gmail.com>
2016-10-06 11:38:09 -0300

    Implemented KNN accuracy measurement

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,56 @@ import weka.core.converters.ConverterUtils.DataSource;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
-/**
- * Created by pablo on 05/10/16.
- */
 public class Main {
 
     public static void main(String[] args) {
 
-        Instances data = null;
+        Instances exemplares = null;
 
         try {
             DataSource source = new DataSource("iris.arff");
-            data = source.getDataSet();
+            exemplares = source.getDataSet();
         } catch (Exception e) {
             System.err.println("There was a problem while reading the file. Exiting...");
             System.exit(1);
         }
 
-        if (!sabemosQualAtributoEhAClasse(data)) {
-            definirClasseComoOUltimoAtributo(data);
+        if (!sabemosQualAtributoEhAClasse(exemplares)) {
+            definirClasseComoOUltimoAtributo(exemplares);
         }
 
-        double distance = distanciaEuclidiana(data.instance(0), data.instance(1));
-        System.out.println(distance);
 
-        System.out.println("Classe do primeiro: " + KNNComUmVizinho(data.instance(0), data));
+        Random random = new Random(34);
+        Instances copiaDosExemplares = new Instances(exemplares);
 
+        copiaDosExemplares.randomize(random);
+
+        int folds = 3;
+
+        copiaDosExemplares.stratify(folds);
+
+        Instances conjuntoDeTreinamento = copiaDosExemplares.trainCV(folds, 0);
+        Instances conjuntoDeTeste = copiaDosExemplares.testCV(folds, 0);
+
+        // calculando a acurácia do 1NN
+
+        int acertos = 0;
+        for (int i = 0; i < conjuntoDeTeste.size(); i++) {
+            Instance exemplar = conjuntoDeTeste.instance(i);
+            String classePrevista = KNNComUmVizinho(exemplar, conjuntoDeTreinamento);
+            String classeReal = exemplar.toString(exemplar.classAttribute());
+
+            if (classePrevista.equals(classeReal)) {
+                acertos++;
+            } else {
+                System.out.println("Errei o exemplar " + exemplar.toString());
+            }
+
+        }
+
+        System.out.println("Acurácia: " + acertos / (double) conjuntoDeTeste.size());
 
     }
 
